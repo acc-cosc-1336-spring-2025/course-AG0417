8523181aa9a4b02a7842d8d4c3af749734be1db0
AG0417 <aaron.graybill@g.austincc.edu>
2025-03-09 04:31:20 +0000

    homework 2 stuff

--- src/homework/c_decisions/main.py
+++ src/homework/c_decisions/main.py
@@ -1,1 +1,12 @@
-#
+import decisions
+
+print("What are the options")
+options = input()
+options = int(options)
+
+print("What is the total")
+total = input()
+total = int(total)
+result = decisions.get_options_ratio(options, total)
+
+print(result)
